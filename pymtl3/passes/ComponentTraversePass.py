7eb8b0735f89b4b76f3f35fbde98d36a0fd7938b
Peitian Pan <pp482@cornell.edu>
2019-08-12 17:04:35 -0400

    Implement component traversal pass

---
+++ pymtl3/passes/ComponentTraversePass.py
@@ -0,0 +1,47 @@
+#=========================================================================
+# ComponentTraversePass.py
+#=========================================================================
+# Traverse the component hierarchy and generates a list of names of
+# components that have the `marked` attribute.
+#
+# Author : Peitian Pan
+# Date   : Aug 1, 2019
+
+from pymtl3 import *
+from pymtl3.passes.BasePass import BasePass, PassMetadata
+
+class ComponentTraversePass( BasePass ):
+
+  def __call__( s, top ):
+    # Make sure we have the pass's namespace
+    if not hasattr(top, "_pass_component_traverse"):
+      top._pass_component_traverse = PassMetadata()
+
+    s.marked = []
+    s.traverse_hierarchy( top )
+
+    # Write result to the pass's namespace
+    top._pass_component_traverse.marked_components = \
+        [c.__repr__() for c in s.marked]
+
+  def traverse_hierarchy( s, component ):
+    """Traverse the component hierarchy and call `s.process` for all components
+    that have `marked` attribute."""
+    if hasattr(component, "marked"):
+      s.process( component )
+
+    for child_component in component.get_child_components():
+      s.traverse_hierarchy( child_component )
+
+  def process( s, component ):
+    """Customized process method to be called for each marked component."""
+    name = component.__repr__()
+    # Get a list of all input/output ports of `component`
+    in_ports = component.get_input_value_ports()
+    out_ports = component.get_output_value_ports()
+    # Get a list of all wires of `component`
+    wires = component.get_wires()
+    # Get a list of two connected signals in the order they are connected
+    connect_order = component.get_connect_order()
+    # Do something here...
+    print(f"{name} {in_ports} {out_ports} {wires} {connect_order}")
---
+++ pymtl3/passes/test/ComponentTraversePass_test.py
@@ -0,0 +1,30 @@
+from itertools import product
+from pymtl3 import *
+from pymtl3.passes.ComponentTraversePass import ComponentTraversePass
+
+class C( Component ):
+  def construct( s, marked = True ):
+    if marked:
+      s.marked = True
+
+class B( Component ):
+  def construct( s, marked = True, marked_c = True ):
+    if marked:
+      s.marked = True
+    s.all_cs = [ C(marked_c) for _ in range(5) ]
+
+class A( Component ):
+  def construct( s, marked = True, marked_b = True, marked_c = True ):
+    if marked:
+      s.marked = True
+    s.all_bs = [ B(marked_b, marked_c) for _ in range(2) ]
+
+def test_all_marked():
+  a = A()
+  a.elaborate()
+  a.apply( ComponentTraversePass() )
+
+def test_not_marked():
+  a = A()
+  a.elaborate()
+  a.apply( ComponentTraversePass() )
