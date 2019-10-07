e663b0b0b0d56b5810a1e8b650d7bd95f54509b7
gnelson85 <36171279+gnelson85@users.noreply.github.com>
2019-10-06 20:33:09 -0400

    Update SvgGenerationPass_test.py
    final commit

--- pymtl3/passes/test/SvgGenerationPass_test.py
+++ pymtl3/passes/test/SvgGenerationPass_test.py
@@ -1,30 +1,63 @@
 from itertools import product
 from pymtl3 import *
-from pymtl3.passes.SvgGenerationPass import SvgGenerationPass
+from SvgGenerationPass import SvgGenerationPass
 
-class C( Component ):
-  def construct( s, marked = True ):
-    if marked:
-      s.marked = True
 
-class B( Component ):
-  def construct( s, marked = True, marked_c = True ):
-    if marked:
-      s.marked = True
-    s.all_cs = [ C(marked_c) for _ in range(5) ]
 
-class A( Component ):
-  def construct( s, marked = True, marked_b = True, marked_c = True ):
-    if marked:
-      s.marked = True
-    s.all_bs = [ B(marked_b, marked_c) for _ in range(2) ]
+class And ( Component ):
+    def construct ( s ):
+        s.a1 = InPort (Bits(1))
+        s.b1 = InPort (Bits(1))
+        s.out1 = OutPort (Bits(1))
+        connect(s.a1,s.out1)
+        
+     
 
-def test_all_marked():
-  a = A()
-  a.elaborate()
-  a.apply( SvgGenerationPass() )
+class Or ( Component ):
+    def construct ( s ):
+        s.a2 = InPort (Bits(1))
+        s.b2 = InPort (Bits(1))
+        s.out2 = OutPort (Bits(1))
+        connect(s.a2,s.out2)
 
-def test_not_marked():
-  a = A()
-  a.elaborate()
-  a.apply( SvgGenerationPass() )
+class Xor ( Component ):
+    def construct ( s ):
+        s.a3 = InPort (Bits(1))
+        s.b3 = InPort (Bits(1))
+        s.out3 = OutPort (Bits(1))
+        connect(s.a3,s.out3)
+   
+
+class FullAdder( Component ):
+    def construct( s ):
+        s.marked =True
+        s.a = InPort  (Bits(1))
+        s.b = InPort  (Bits(1))
+        s.ci = InPort (Bits(1))
+        s.co = OutPort (Bits(1))
+        s.sum = OutPort (Bits(1)) 
+        
+        s.a_xor_b = Xor()
+        s.a_and_b = And()
+        s.ab_and_ci = And()
+        s.ab_xor_ci = Xor()
+        s. abxor_or_aband = Or()
+
+        connect(s.a,  s.a_xor_b.a3)
+        connect(s.b,  s.a_xor_b.b3)
+        connect(s.a,  s.a_and_b.a1)
+        connect(s.b,  s.a_and_b.b1)
+        connect( s.a_xor_b.out3, s.ab_and_ci.a1)
+        connect( s.ci, s.ab_and_ci.b1)
+        connect( s.a_xor_b.out3, s.ab_xor_ci.a3)
+        connect( s.ci, s.ab_xor_ci.b3)
+        connect( s.ab_and_ci.out1, s.abxor_or_aband.a2)
+        connect( s.a_and_b.out1, s.abxor_or_aband.b2)
+        connect(s.abxor_or_aband.out2, s.co)
+        connect ( s.ab_xor_ci.out3, s.sum)
+
+  
+def test_adder():
+  adder = FullAdder()
+  adder.elaborate()
+  adder.apply( SvgGenerationPass() )
